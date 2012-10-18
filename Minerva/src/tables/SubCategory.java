9b89b26a0a187b3abf4de78d523c6a43f7a1f57c
Joacim André Jakobsen <joacimjakobsen@gmail.com>
2012-10-18 14:13:41 +0200

    Test

---
+++ Minerva/src/tables/SubCategory.java
@@ -0,0 +1,25 @@
+package tables;
+
+public class SubCategory {
+	private long id;
+	private String name;
+
+	public SubCategory() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	private void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
