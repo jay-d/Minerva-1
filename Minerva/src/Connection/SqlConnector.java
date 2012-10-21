a283089a03508085d1cf6bef247ffe022396d041
Rolf Andreas <bad_boit@msn.com>
2012-10-21 19:27:05 +0200

    oppdatering

--- Minerva/src/Connection/SqlConnector.java
+++ Minerva/src/Connection/SqlConnector.java
@@ -1,18 +1,26 @@
 package Connection;
-
+//
 //public class SqlConnector {
-//	
-//	public Create(Object object) {
-//		SessionFactory sessionFactory = config.buildSessionFactory(); //fjernes senere nå vi lager hibernate klasse
-//		Session session = sessionFactory.getCurrentSession();
-//		//Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-//	    session.beginTransaction();
-//	    
-//	    session.save(object);
-//	    session.getTransaction().commit();
-//	}
-//	
-//	public Update(Object object) {
+//
+//public create(Object object) {
+//	Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+//	//Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+//    session.beginTransaction();
+//    
+//    session.save(object);
+//    session.getTransaction().commit();
+//}
+//
+//public update(Object object) {
+//
+//}
+//public insert(Object object) {
+//
+//}
+//public get(Object object) {
+//
+//}
+//public delete(Object object) {
 //
 //}
 //}
