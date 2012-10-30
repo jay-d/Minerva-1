c5772f1af91f17de5299f14afdabe0f21ac93c2e
Baard Helland Boe <helland.boe@gmail.com>
2012-10-30 22:38:48 +0100

    legger til language filer

---
+++ Minerva/src/lang/LangEng.java
@@ -0,0 +1,22 @@
+package lang;
+
+public class LangEng {
+
+	
+	public static final String 
+	homeIntroHeader ="Welcome to Minerva!", 
+	homeIntroText ="Minerva is a webpage for people who want to get new knowledge!",
+	homeUpdate1Header = "Update nr. 1 17.10.2012",
+	homeUpdate1Text = "Topic chooseren er lagt til!",
+	homeUpdate2Header = "Update nr. 2 18.10.2012",
+	homeUpdate2Text = " blablablablabl ablablablablablablabla blablabla blablab lablablablablabl ablablablabl ablablablablablabla" ,
+	homeUpdate3Header = "",
+	homeUpdate3Text = ""
+	
+	;
+	
+	
+	
+	public LangEng (){
+	}
+}
