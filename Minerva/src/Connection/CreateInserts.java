aa3505c7e2f7c1568dbb2b4aea550e9a0feb1888
Andreas Nesheim <andreas.aronsen.nesheim@gmail.com>
2012-11-01 11:35:18 +0100

    endret pa filer

---
+++ Minerva/src/Connection/CreateInserts.java
@@ -0,0 +1,69 @@
+package Connection;
+
+import org.hibernate.Session;
+
+import tables.Profile;
+import tables.User;
+
+public class CreateInserts {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Dette funke isje");
+		createSubCategories();
+	}
+
+	private static void createUsers() {
+		ProfileCon.createUser("test45", 567);
+
+	}
+	
+	private static void createMainCategories(){
+		CategoryCon.createMainCategory("Programmering");
+		CategoryCon.createMainCategory("Netverk");
+		CategoryCon.createMainCategory("Annet");
+	}
+	
+	private static void createSubCategories(){
+		CategoryCon.createSubCategory("Java", 2);
+		CategoryCon.createSubCategory("TCP", 3);
+		CategoryCon.createSubCategory("tull", 4);
+	}
+
+	public static void createProfilesEkstra(){
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+
+		Profile daniel = new Profile();
+		daniel.setFirstName("Daniel");
+		daniel.setLastName("Jajevski");
+		daniel.setInformation("blabla-informasjon!!!!!");
+		daniel.setLocation("Stavanger");
+		
+		Profile rolf = new Profile();
+		rolf.setFirstName("Rolf");
+		rolf.setLastName("Boiten");
+		rolf.setInformation("test");
+		rolf.setLocation("Gjerstad");
+		
+		Profile bard = new Profile();
+		bard.setFirstName("Bård");
+		bard.setLastName("Helland");
+		bard.setInformation("enda en test");
+		bard.setLocation("Stavanger");
+		
+		Profile andy = new Profile();
+		andy.setFirstName("Andreas");
+		andy.setLastName("Nesheim");
+		andy.setInformation("test igjen");
+		andy.setLocation("Tasta");
+		
+		session.save(daniel); // doesn't save here
+		session.save(rolf); // doesn't save here
+		session.save(bard); // doesn't save here
+		session.save(andy); // doesn't save here
+		session.getTransaction().commit();
+	}
+}
